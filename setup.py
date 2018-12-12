34a472dba49e3431b1f35ff6ad5f35a2fcbb493e
Aanu-A <aanuabayom@gmail.com>
2018-12-12 10:39:02 -0800

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A test python package',
-	url='https://github.com/khalad-hasan/myMileConverter',
+	url='https://github.com/Aanu-A/AAmyMilePackaging.git',
     author='A Abayomi',
     author_email='aanuabayom@gmail.com'
 )
